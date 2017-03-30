d70fe8c49194076e86e1bd68091e1129408f6d7c
simon.schulz <simon.schulz@active-group.de>
2017-03-30 09:11:33 +0200

    Remove compiler warnings for elixir 1.4, by removing brackets

--- mix.exs
+++ mix.exs
@@ -2,27 +2,30 @@ defmodule Drempel.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :drempel,
-     version: "0.1.0",
-     source_url: "https://github.com/frobese/drempel",
-     elixir: "~> 1.2",
-     package: package,
-     description: description,
-     docs: docs,
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test],
-     deps: deps]
+    [
+      app: :drempel,
+      version: "0.1.0",
+      source_url: "https://github.com/frobese/drempel",
+      elixir: "~> 1.2",
+      package: package(),
+      description: description(),
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test],
+      deps: deps()
+    ]
   end
 
   def application do
-    [mod: {Drempel.App, []},
-     applications: [:logger]]
+    [mod: {Drempel.App, []}, applications: [:logger]]
   end
 
   defp package do
-    [maintainers: ["Christian Zuckschwerdt"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/frobese/drempel"}]
+    [
+      maintainers: ["Christian Zuckschwerdt"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/frobese/drempel"}
+    ]
   end
 
   defp description do
@@ -37,11 +40,13 @@ defmodule Drempel.Mixfile do
   end
 
   defp deps do
-    [{:phoenix, "~> 1.1", optional: true},
-     {:excoveralls, "~> 0.5", only: :test},
-     {:credo, "~> 0.3", only: [:dev, :test]},
-     {:dogma, "~> 0.1", only: :dev},
-     {:earmark, "~> 0.2", only: :dev},
-     {:ex_doc, "~> 0.11", only: :dev}]
+    [
+      {:phoenix, "~> 1.1", optional: true},
+      {:excoveralls, "~> 0.5", only: :test},
+      {:credo, "~> 0.3", only: [:dev, :test]},
+      {:dogma, "~> 0.1", only: :dev},
+      {:earmark, "~> 0.2", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
   end
 end
